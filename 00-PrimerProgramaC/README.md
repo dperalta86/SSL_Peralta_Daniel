4d7306fff9451e54b909591f45c17064b2b362ac
dperalta <dp25443@gmail.com>
2025-04-01 00:16:09 -0300

    ADD README y primerarchivo

---
+++ 00-PrimerProgramaC/README.md
@@ -0,0 +1,3 @@
+#Imprimir Cartel Bienvenida
+
+El siguiente programa (desarrollado en C) imprime por pantalla cartel de bienvenida.
---
+++ 00-PrimerProgramaC/primerarchivo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    for (int i=0; i<80;i++){
+        if (i==0)
+        printf(" =");
+        else
+        printf("=");
+    }
+    printf("\n|                                                                                |");
+    printf("\n|                           *** Bienvenido a ***                                 |");
+    printf("\n|                                                                                |");    
+    printf("\n|                   SINTÁXIS Y SEMÁNTICA DE LOS LENGUAJES                        |");
+    printf("\n|                                                                                |\n");
+    for (int i=0; i<80;i++){
+        if (i==0)
+        printf(" =");
+        else
+        printf("=");
+    }
+    printf("\n\n\n\n\n");
+}
